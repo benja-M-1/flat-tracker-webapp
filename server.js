31fe5a08d762c50b92d928ed41407bc0096c4a11
Benjamin Grandfond <benjamin.grandfond@gmail.com>
2015-03-29 18:10:11 +0200

    Add server file to run on heroku

---
+++ server.js
@@ -0,0 +1,8 @@
+var gzippo = require('gzippo');
+var express = require('express');
+var morgan = require('morgan');
+var app = express();
+
+app.use(morgan('dev'));
+app.use(gzippo.staticGzip("" + __dirname + "/www"));
+app.listen(process.env.PORT || 8000);
